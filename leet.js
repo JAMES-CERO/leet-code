d81a60b4acc37d1f8805c3492573a0d5d4959ab2
James Aules <james_aules@icloud.com>
2023-03-15 16:57:21 -0400

    Reverse an integer 32-bit limit

--- leet.js
+++ leet.js
@@ -46,10 +46,26 @@
 // palindrome
 // , and false otherwise.
 
-const PalindromeNum = (x) => {
-    return x == x.toString().split('').reverse().join('');
-}
-//we want to check if both sides are equal regardless of their type. In this case, X is a number while the computed value is a string
+// const PalindromeNum = (x) => {
+//     return x == x.toString().split('').reverse().join('');
+// }
+// //we want to check if both sides are equal regardless of their type. In this case, X is a number while the computed value is a string
 
-let test = PalindromeNum(343)
-console.log(test)
+// let test = PalindromeNum(343)
+// console.log(test)
+
+/////////////////////////Reverse an integer / 32 bitd limit//////////////
+
+// var reverse = function(x) {
+//     if (x < 0) return -1 * reverse(-x);
+//     const r = parseInt(`${x}`.split('').reverse().join(''), 10);
+//     if (r > 2 ** 31 - 1) return 0;
+    
+//     return r;
+//   };
+//// without the limit 
+
+// Arrow function for reversing the number
+// const reverseNumber = number => parseFloat(number.toString().split('').reverse().join('')) * Math.sign(number)
+
+// console.log(reverseNumber(12345))
